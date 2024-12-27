205453cd325f6e63c91a217f7209fd296cc87f47
EpicET <etowner@middlebury.edu>
2024-12-26 22:56:25 -0500

    initial commit

---
+++ backend/src/main/java/com/app/bank/model/Transaction.java
@@ -0,0 +1,24 @@
+package com.app.bank.model;
+
+public class Transaction {
+    private String type;
+    private float amount;
+
+    public Transaction(String type, float amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + amount;
+    }
+}
