c60df199aa53304b02e7f51efa36c48c834a17a8
EpicET <etowner@middlebury.edu>
2025-03-20 17:15:00 -0400

    Updated Home.js to apply UserContext

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Card, Col, Container, Row, Alert } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { api } from "../../api/axiosConfig";
@@ -8,48 +8,35 @@ import Header from "./Header";
 import "./Home.css";
 import OpenAccount from "./OpenAccount";
 import PieChart from "./PieChart";
+import { UserContext } from "../../UserContext";
 
 const Home = () => {
-  const [user, setUser] = useState([]);
+  const { user, getUser, setUser} = useContext(UserContext)
   const { userID } = useParams(); // This is the userID from the URL
   const [password, setPassword] = useState();
   const [accList, setAccList] = useState([]);
   const [error, setError] = useState();
 
-  const updateUser = (updatedUser) => {
-    setUser(updatedUser);
-  };
 
-  function openAcc(type) {
+  const openAcc = async (type) => {
     if( accList.length >= 3){
       setError("You can only have 3 accounts");
       return;
     }
+    try {
+      const response = await api.post(`/api/v1/user/${userID}/${type}`);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
 
-    api
-      .post(`/api/v1/user/${userID}/${type}`) // Returns a string
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+    getUser(userID); 
+  };
 
   useEffect(() => {
-    const getUser = () => { // Returns a user
-      api
-        .get(`/api/v1/user/${userID}`)
-        .then((response) => {
-          setUser(response.data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    };
-
-    getUser();
-  }, [userID]); // Eventually will need to update page when accList changes
+    getUser(userID);
+  }, [userID]);
+  
 
 
   useEffect(() => {
@@ -62,7 +49,7 @@ const Home = () => {
         }
       });
     }
-    // console.log(user);
+    // console.log("Deconstruct user:", user);
   }, [user]);
 
   return (
@@ -77,12 +64,12 @@ const Home = () => {
           <Col key={1}>
             <Card border="dark">
               <AccList userID={userID} accList={accList} />
-              <OpenAccount userID={userID} openAcc={openAcc} setError={setError} setUser={setUser} />
+              <OpenAccount userID={userID} openAcc={openAcc} setError={setError} />
               {error && <Alert className="" variant="danger">{error}</Alert>}
             </Card>
           </Col>
           <Col key={2}>
-            <Transfer user={user} updateUser={updateUser} />
+            <Transfer userID={userID} setUser={ setUser} />
           </Col>
           <Col key={3}>
             <Card style={{ width: "32rem" }} className="mb-2">
